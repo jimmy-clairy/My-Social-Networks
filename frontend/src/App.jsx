207eb0901072fb4acffea53e2b432b0a250031e4
Jimmy Clairy <jimmyclairy@gmail.com>
2024-03-14 12:03:43 +0100

    Add Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <div className="container-main">
+            <h1 className=" bg-red-400 text-2xl px-4 py-2 rounded mt-4">
+                Hello world!
+            </h1>
+        </div>
+    )
+}
