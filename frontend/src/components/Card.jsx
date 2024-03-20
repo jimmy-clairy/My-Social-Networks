a64b4f4fe9637e3aea01494f2dd27545e8cbf01e
Jimmy Clairy <jimmyclairy@gmail.com>
2024-03-20 21:24:17 +0100

    Add trash if authorization

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,14 +1,17 @@
+import { useContext } from "react";
+import { Context } from "../context/Context";
 import { BsTrash3 } from "react-icons/bs";
 
 export default function Card({ post, deletePost }) {
-
+    const { userCTX } = useContext(Context)
+    console.log(userCTX);
     return (
         <div className="mt-4 mx-auto max-w-2xl p-2 text-2xl bg-cyan-500 shadow-lg shadow-cyan-500/50 rounded flex gap-2">
             <img className="rounded" src={post.picture} alt="" width={240} />
             <div>
                 <p>{post.posterPseudo}</p>
                 <p>{post.message}</p>
-                <BsTrash3 onClick={() => deletePost(post._id)} className=" cursor-pointer" />
+                {(userCTX.ifAdmin || userCTX._id === post.posterId) && <BsTrash3 onClick={() => deletePost(post._id)} className=" cursor-pointer" />}
             </div>
         </div>
     )
