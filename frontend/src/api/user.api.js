566e7f8e4ada05deebfdb7b1e137a797ca3b15fd
Jimmy Clairy <jimmyclairy@gmail.com>
2024-03-21 11:04:40 +0100

    Add updateOneUser

--- frontend/src/api/user.api.js
+++ frontend/src/api/user.api.js
@@ -26,3 +26,24 @@ export const getUser = async (userId, token) => {
         throw error;
     }
 }
+
+
+export const updateOneUser = async (userId, token, formData) => {
+
+    const url = USER_URL + userId;
+
+    const options = {
+        method: 'PUT',
+        headers: { 'Authorization': `Bearer ${token}` },
+        body: formData
+    };
+
+    try {
+        const data = await fetchData(url, options)
+
+        return data
+    } catch (error) {
+        console.error('Erreur lors de la modification de l\'utilisateur :', error)
+        throw error;
+    }
+}
