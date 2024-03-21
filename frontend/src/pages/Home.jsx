9c030fa110454dd3a3985495ed3a1c76dde84659
Jimmy Clairy <jimmyclairy@gmail.com>
2024-03-21 12:09:03 +0100

    Delete save in context

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,27 +1,15 @@
-import { useContext, useEffect } from "react"
+import { useContext } from "react"
 import { Context } from "../context/Context";
 import Card from "../components/Card";
 import FormAddPost from "../components/FormAddPost";
-import { getUser } from "../api/user.api";
-import { deleteOnePost, getAllPosts } from "../api/post.api";
+import { deleteOnePost } from "../api/post.api";
 import { getLocaleStorage } from "../utils/localeStorage";
 
 export default function Home() {
-    const { setUserCTX, postsCTX, setPostsCTX } = useContext(Context)
+    const { postsCTX, setPostsCTX } = useContext(Context)
 
     const userId = getLocaleStorage('userId')
     const token = getLocaleStorage('token')
-
-    useEffect(() => {
-        async function getInfo() {
-            if (userId && token) {
-                setUserCTX(await getUser(userId, token))
-                setPostsCTX(await getAllPosts())
-            }
-        }
-        getInfo()
-    }, [])
-
 
     async function deletePost(id) {
         const postsFilter = postsCTX.filter((post) => post._id !== id)
