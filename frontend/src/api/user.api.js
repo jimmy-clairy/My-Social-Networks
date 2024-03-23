4f446b66f0755339a70ddefc83c644b972331f08
Jimmy Clairy <jimmyclairy@gmail.com>
2024-03-23 09:07:50 +0100

    Add deleteOneUser and js doc

--- frontend/src/api/user.api.js
+++ frontend/src/api/user.api.js
@@ -27,9 +27,15 @@ export const getUser = async (userId, token) => {
     }
 }
 
-
+/**
+ * Updates a user's information.
+ * @param {string} userId - The ID of the user to update.
+ * @param {string} token - The authorization token.
+ * @param {FormData} formData - The form data containing the updated user information.
+ * @returns {Promise<Object>} - A promise resolving to the updated user data.
+ * @throws {Error} - If an error occurs during the update process.
+ */
 export const updateOneUser = async (userId, token, formData) => {
-
     const url = USER_URL + userId;
 
     const options = {
@@ -39,11 +45,36 @@ export const updateOneUser = async (userId, token, formData) => {
     };
 
     try {
-        const data = await fetchData(url, options)
+        const data = await fetchData(url, options);
 
-        return data
+        return data;
     } catch (error) {
-        console.error('Erreur lors de la modification de l\'utilisateur :', error)
+        console.error('Erreur lors de la modification de l\'utilisateur :', error);
+        throw error;
+    }
+}
+
+/**
+ * Deletes a user.
+ * @param {string} userId - The ID of the user to delete.
+ * @param {string} token - The authorization token.
+ * @returns {Promise<Object>} - A promise resolving to the result of the deletion operation.
+ * @throws {Error} - If an error occurs during the deletion process.
+ */
+export const deleteOneUser = async (userId, token) => {
+    const url = USER_URL + userId;
+
+    const options = {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` }
+    };
+
+    try {
+        const data = await fetchData(url, options);
+
+        return data;
+    } catch (error) {
+        console.error('Erreur lors de la suppression de l\'utilisateur :', error);
         throw error;
     }
 }
